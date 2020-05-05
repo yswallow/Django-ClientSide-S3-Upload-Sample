a72da9dc8c9e4bf6d0434110fcd54fbf9f4fbe5a
Yu Sanagi <yswallow@example.com>
2020-05-05 18:40:43 +0900

    submit_formページの廃止

--- hello/urls.py
+++ hello/urls.py
@@ -5,6 +5,6 @@ from hello import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("sign_s3", views.sign_s3, name="sign_s3"),
-    path("submit_form", views.submit_form, name="submit_form"),
+    #path("submit_form", views.submit_form, name="submit_form"),
     path("get_image_url", views.get_image_url, name="get_image_url"),
 ]
